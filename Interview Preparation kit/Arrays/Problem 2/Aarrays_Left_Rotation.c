564c47ae2a6060a019765a94e39a7759f0052fd1
Kasala Fahadh <67508953+Fahadh4444@users.noreply.github.com>
2020-12-10 19:06:04 +0530

    Uploaded Arrays_Left_Rotation Problem

---
+++ Interview Preparation kit/Arrays/Problem 2/Aarrays_Left_Rotation.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+
+int main()
+{
+	int size,rotations;
+	scanf("%d",&size);
+	scanf("%d",&rotations);
+	
+	int array[size];
+	 for(int i = 0; i<size; i++)
+	 {
+	 	scanf("%d",&array[i]);
+	 }
+	 
+	 
+	 for(int i= 0; i<rotations; i++)
+	 {
+	 	int temp = array[0];
+	 	for(int j = 0; j<size; j++)
+	 	{
+		 	array[j] = array [j+1];
+		}
+		array[size-1] = temp;
+	 }
+	 
+	 for(int i = 0; i<size; i++)
+	 {
+	 	printf("%d ",array[i]);
+	 }
+	 
+}
